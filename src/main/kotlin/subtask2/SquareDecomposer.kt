cec6d45e188114257b6ecd012ee21c036d03f724
avm82 <1982alexvm@gmail.com>
2021-07-07 16:46:55 +0300

    feat: SquaresTest done

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -2,8 +2,21 @@ package subtask2
 
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        return findSquareSum(number = number, square = number.toLong() * number.toLong())
+    }
+
+    private fun findSquareSum(number: Int, square: Long): Array<Int>? {
+        var supposed = number - 1
+        while (supposed > 0) {
+            val squareDiff = square - supposed.toLong() * supposed.toLong()
+            if (squareDiff == 0L) return arrayOf(supposed)
+            if (squareDiff > 0) {
+                val findSquareSum = findSquareSum(supposed, squareDiff)
+                if (findSquareSum != null) return findSquareSum.plus(supposed)
+            }
+            supposed--
+        }
+        return null
     }
 }
