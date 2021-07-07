f746cca86a1729aa02f89828129477a4cbfe6091
avm82 <1982alexvm@gmail.com>
2021-07-07 18:50:33 +0300

    feat: TelephoneFinder done

--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -2,8 +2,32 @@ package subtask3
 
 class TelephoneFinder {
 
-    // TODO: Complete the following function
+    companion object {
+        val neighborsList = mapOf(
+            '1' to arrayOf('2', '4'),
+            '2' to arrayOf('1', '3', '5'),
+            '3' to arrayOf('2', '6'),
+            '4' to arrayOf('1', '5', '7'),
+            '5' to arrayOf('2', '6', '8', '4'),
+            '6' to arrayOf('3', '5', '9'),
+            '7' to arrayOf('4', '8'),
+            '8' to arrayOf('7', '5', '9', '0'),
+            '9' to arrayOf('6', '8'),
+            '0' to arrayOf('8')
+
+        )
+    }
+
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+        if (!number.matches(Regex("[0-9]*"))) return null
+        val result = mutableListOf<String>()
+        number.forEachIndexed { index, char ->
+            run {
+                neighborsList[char]?.forEach {
+                    result.add(number.replaceRange(index, index + 1, it.toString()))
+                }
+            }
+        }
+        return result.toTypedArray()
     }
 }
