f0db2656522eaa9ab332ba4265a801732d4ce713
avm82 <1982alexvm@gmail.com>
2021-07-07 16:54:06 +0300

    feat: SquaresTest refactored

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -3,16 +3,17 @@ package subtask2
 class SquareDecomposer {
 
     fun decomposeNumber(number: Int): Array<Int>? {
-        return findSquareSum(number = number, square = number.toLong() * number.toLong())
+        return findSquareSequence(number = number, square = number.toLong() * number.toLong())
     }
 
-    private fun findSquareSum(number: Int, square: Long): Array<Int>? {
+    private fun findSquareSequence(number: Int, square: Long): Array<Int>? {
+        if(square in 2..3) return null
         var supposed = number - 1
         while (supposed > 0) {
             val squareDiff = square - supposed.toLong() * supposed.toLong()
             if (squareDiff == 0L) return arrayOf(supposed)
             if (squareDiff > 0) {
-                val findSquareSum = findSquareSum(supposed, squareDiff)
+                val findSquareSum = findSquareSequence(supposed, squareDiff)
                 if (findSquareSum != null) return findSquareSum.plus(supposed)
             }
             supposed--
